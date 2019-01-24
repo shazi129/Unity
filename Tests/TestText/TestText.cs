7fd9cdf52758188f7ac415e6d7cc1015822b131e
zhangwen <i.am.zw129@gmail.com>
2019-01-24 15:36:51 +0800

    增加TestText

---
+++ Tests/TestText/TestText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Text t = gameObject.GetComponent<Text>();
+        if (t != null)
+        {
+            t.text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbb";
+        }
+	}
+}
